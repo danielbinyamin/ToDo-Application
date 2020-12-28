13d9129d019fd06d02b0f83c8d36ea29582216be
danielbinyamin <danielbinyamin@gmail.com>
2020-12-28 09:11:29 +0200

    added path var endpoint

---
+++ restful-web-services/src/main/java/com/todo/rest/webservices/restfulwebservices/HelloWorldBean.java
@@ -0,0 +1,26 @@
+package com.todo.rest.webservices.restfulwebservices;
+
+public class HelloWorldBean {
+
+	private String message;
+
+	public HelloWorldBean(String string) {
+		this.message = string;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public String toString() {
+		return "HelloWorldBean [message=" + message + "]";
+	}
+	
+	
+
+}
--- restful-web-services/src/main/java/com/todo/rest/webservices/restfulwebservices/HelloWorldController.java
+++ restful-web-services/src/main/java/com/todo/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -1,16 +1,25 @@
 package com.todo.rest.webservices.restfulwebservices;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 
-	@RequestMapping(method=RequestMethod.GET, path = "/hello-world")
-	public String hellWorld() {
+	@GetMapping(path = "/hello-world")
+	public String helloWorld() {
 		return "Hello World";	
 	}
 	
+	@GetMapping(path = "/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello world Bean");	
+	}
+	
+	@GetMapping(path = "/hello-world/path-var/{name}")
+	public HelloWorldBean helloWorldBean(@PathVariable String name) {
+		return new HelloWorldBean("Hello world: " + name);	
+	}
 	
 }
